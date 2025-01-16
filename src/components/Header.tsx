dea20177c415ce2942b2fd1c24220dfb1552d3b1
caoguangyao <caoguangyao28@163.com>
2025-01-16 19:29:56 +0800

    feat: 优化样式

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,9 +16,12 @@ export default function Header() {
   return (
     <div className= 'fixed w-full z-10'>
       <header className={'container flex items-center justify-between text-white mx-auto pl-3 pr-3 pt-5 pb-5'}>
-        <Link className={'flex items-center justify-center w-[50px] h-[50px] bg-amber-100 rounded-2xl'} href="/">
-          <Image className={'w-[40px]'} src={LogoSrc} alt={'logo'} />
-        </Link>
+        <div className={'flex min-w-10 space-x-2 items-center'}>
+          <Link className={'flex items-center justify-center w-[50px] h-[50px] bg-amber-100 rounded-2xl'} href="/">
+            <Image className={'w-[40px]'} src={LogoSrc} alt={'logo'}/>
+          </Link>
+          <span className={'text-3xl'}> Shinecgy coding</span>
+        </div>
         <div className={'text-xl space-x-4 '}>
           {linkData.map((item, index) => (
             <Link
